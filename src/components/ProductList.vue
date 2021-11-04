a21b146ed9089eee088e5ccf36c9111a6f53f22a
Rodion Sibov <rodionsibov@gmail.com>
2021-11-04 11:46:28 +0200

    init

---
+++ src/components/ProductList.vue
@@ -0,0 +1,26 @@
+<template>
+  <ProductCard
+    v-for="product in products"
+    :key="product.id"
+    :product="product"
+  />
+</template>
+
+<script>
+import ProductCard from "@/components/ProductCard.vue";
+
+export default {
+  components: { ProductCard },
+  computed: {
+      products() {
+          return this.$store.state.products
+      }
+  },
+  mounted() {
+    this.$store.dispatch("getProducts");
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/Product.vue
@@ -0,0 +1,19 @@
+<template>
+product
+  <!-- <ProductCard :product="product" /> -->
+</template>
+
+<script>
+// import ProductCard from "@/components/ProductCard.vue";
+
+export default {
+  computed: {
+      product() {
+          return this.$store.state.product
+      }
+  },
+  components: {
+    // ProductCard,
+  },
+};
+</script>
